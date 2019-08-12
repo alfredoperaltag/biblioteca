e7d8d13b54ae879e4a31bd8b92df0594172a9a07
alfredoperaltag <alfredo08peralta@gmail.com>
2019-08-11 21:27:04 -0500

    index.html

---
+++ src/main/resources/static/controladores/indexController.js
@@ -0,0 +1,20 @@
+/* let index = angular.module("biblioteca", ["ngRoute"], function ($compileProvider) {
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|javascript):/);
+});
+index.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+        .when("/login", {
+            templateUrl: "/vistas/index/login.html"
+        }).when("/registro", {
+            templateUrl: "/vistas/index/registro.html"
+        }).when("/recuperacion", {
+            templateUrl: "/vistas/index/recuperacion.html"
+        }).otherwise({
+            templateUrl: "/vistas/index/login.html"
+        });
+}]);
+
+index.controller("indexController", function ($scope) {
+    $scope.minombre = "Alfredo Peralta"
+}) */
